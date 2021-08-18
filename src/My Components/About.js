310301bd0b56cabde31155d2ed3bc3ffbd324b35
Rohit <tailorrohit06@gmail.com>
2021-08-18 14:08:39 +0530

    first commit

---
+++ src/My Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>This is Todo app , which is use to create you a todo list.</p>
+        </div>
+    )
+}
